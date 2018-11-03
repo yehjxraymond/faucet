ff8701d0c7c2b87397e002d56c380eddb99a8e1d
Raymond Yeh <yehjxraymond@gmail.com>
2018-11-04 00:05:11 +0800

    Added jest

--- dist/client.js
+++ dist/client.js
@@ -8,31 +8,4 @@ client.Fund({ address: "user" }, (err, response) => {
         console.log(err);
     console.log('Success:', response.success);
 });
-/*
-const grpc = require('grpc');
-const protoLoader = require('@grpc/proto-loader');
-
-const PROTO_PATH = __dirname + '/faucet.proto';
-
-const packageDefinition = protoLoader.loadSync(
-  PROTO_PATH,
-  {
-    keepCase: true,
-    longs: String,
-    enums: String,
-    defaults: true,
-    oneofs: true
-  });
-const faucetProto = grpc.loadPackageDefinition(packageDefinition);
-
-function main() {
-  const client = new faucetProto.Faucet('localhost:50051', grpc.credentials.createInsecure());
-
-  client.Fund({ address: "user" }, function (err, response) {
-    console.log('Success:', response.success);
-  });
-}
-
-main();
-*/ 
 //# sourceMappingURL=client.js.map
--- src/client.ts
+++ src/client.ts
@@ -8,31 +8,3 @@ client.Fund({ address: "user" }, (err: Error, response: any) =>{
   if(err) console.log(err);
   console.log('Success:', response.success);
 });
-
-/*
-const grpc = require('grpc');
-const protoLoader = require('@grpc/proto-loader');
-
-const PROTO_PATH = __dirname + '/faucet.proto';
-
-const packageDefinition = protoLoader.loadSync(
-  PROTO_PATH,
-  {
-    keepCase: true,
-    longs: String,
-    enums: String,
-    defaults: true,
-    oneofs: true
-  });
-const faucetProto = grpc.loadPackageDefinition(packageDefinition);
-
-function main() {
-  const client = new faucetProto.Faucet('localhost:50051', grpc.credentials.createInsecure());
-
-  client.Fund({ address: "user" }, function (err, response) {
-    console.log('Success:', response.success);
-  });
-}
-
-main();
-*/
